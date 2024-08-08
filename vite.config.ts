ebf169ca3c2a2a9ed210b9af329922305308e49a
petrunkito <147442266+petrunkito@users.noreply.github.com>
2024-08-08 12:49:12 -0600

    d

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	base:"https://petrunkito.github.io/Memory-Game/"
 });
