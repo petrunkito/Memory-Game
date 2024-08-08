042a09f586c8e2416058af400efeff193d2c0e21
petrunkito <147442266+petrunkito@users.noreply.github.com>
2024-08-08 11:34:58 -0600

    agregando el readme al ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	base: "/Memory-Game/"
 });
